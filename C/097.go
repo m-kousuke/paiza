ca4e1ed5412905c5298c0f30fa3b4d295367653e
m-kousuke <e155415y@st.u-gakugei.ac.jp>
2023-01-29 17:11:50 +0900

    C097:プレゼント応募企画の実施

---
+++ C/097.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	N int
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	input := strings.Split(sc.Text(), " ")
+	N, _ := strconv.Atoi(input[0])
+	X, _ := strconv.Atoi(input[1])
+	Y, _ := strconv.Atoi(input[2])
+
+	for i := 1; i <= N; i++ {
+		if i%getAB(X, Y) == 0 {
+			fmt.Println("AB")
+			continue
+		}
+		if i%X == 0 {
+			fmt.Println("A")
+			continue
+		}
+		if i%Y == 0 {
+			fmt.Println("B")
+			continue
+		}
+		fmt.Println("N")
+	}
+}
+
+func getAB(X int, Y int) int {
+	if X%Y == 0 {
+		return X
+	}
+	if Y%X == 0 {
+		return Y
+	}
+	return X * Y
+}
